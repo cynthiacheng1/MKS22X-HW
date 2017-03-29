f2010420ebdad27bbc0a08af191ce8708eea76ae
Cynthia Cheng <cynthiacheng1022@gmail.com>
2017-03-29 01:17:13 -0400

    got rid of print statments

--- 08Merge/Merge.java
+++ 08Merge/Merge.java
@@ -24,32 +24,32 @@ public class Merge{
 		int bIndex = 0;
 
 		while (aIndex <a.length && bIndex<b.length){
-			System.out.println(aIndex);
-			System.out.println(bIndex);
+			//System.out.println(aIndex);
+			//System.out.println(bIndex);
 			if (a[aIndex] >= b[bIndex]){
 				destination[aIndex+bIndex] = b[bIndex];
 				bIndex++;
 			}
 			else{
-				System.out.println(aIndex);
-				System.out.println(bIndex);
+				//System.out.println(aIndex);
+				//System.out.println(bIndex);
 				destination[aIndex+bIndex] = a[aIndex];
 				aIndex++;
-				System.out.println(aIndex);
-				System.out.println(bIndex);
+				//System.out.println(aIndex);
+				//System.out.println(bIndex);
 			}
 		}
 		if (aIndex == a.length){
 			while (bIndex < b.length){
-				System.out.println(aIndex);
-				System.out.println(bIndex);
+				//System.out.println(aIndex);
+				//System.out.println(bIndex);
 				destination[aIndex+bIndex] = b[bIndex];
 				bIndex++;
 			}
 		}
 		else{
 			while (aIndex < a.length){
-				System.out.println("WENT THRU");
+				//System.out.println("WENT THRU");
 				destination[aIndex+bIndex] = a[aIndex];
 				aIndex++;
 			}
@@ -57,16 +57,16 @@ public class Merge{
 
 	}
 
-	public static void main(String[]a){
-		int[]y = {3,6,9,12,35};
-		int[]x = {0,2,3,5,6};
-		int[]z = new int[10];
-		merge(x,y,z);
-		System.out.println(Arrays.toString(z));
-		int[]b = {3,5,7,8,3,2,24,7658,24,1346,7865};
-		mergesort(b);
-		System.out.println(Arrays.toString(b));
-	}
+	// public static void main(String[]a){
+	// 	int[]y = {3,6,9,12,35};
+	// 	int[]x = {0,2,3,5,6};
+	// 	int[]z = new int[10];
+	// 	merge(x,y,z);
+	// 	System.out.println(Arrays.toString(z));
+	// 	int[]b = {3,5,7,8,3,2,24,7658,24,1346,7865};
+	// 	mergesort(b);
+	// 	System.out.println(Arrays.toString(b));
+	// }
 
 	
 }
